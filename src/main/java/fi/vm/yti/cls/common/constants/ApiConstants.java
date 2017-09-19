b220ebb29a4103348ea25735be3f6e4eb216f8e9
Miikka Sauramo <miikka.sauramo@phz.fi>
2017-09-19 15:40:46 +0300

    Update Spring Boot version to 1.5.7 Merge two classes with similar content

---
+++ src/main/java/fi/vm/yti/cls/common/constants/ApiConstants.java
@@ -0,0 +1,20 @@
+package fi.vm.yti.cls.common.constants;
+
+public class ApiConstants {
+    private ApiConstants() {}
+
+    public static final String API_VERSION = "v1";
+
+    public static final String API_BASE_PATH = "/api";
+
+    public static final String API_CONTEXT_PATH_INTAKE = "/";
+    
+    public static final String API_CONTEXT_PATH_RESTAPI = "";
+
+    public static final String API_PATH_CODEREGISTRIES = "/coderegistries";
+
+    public static final String API_PATH_CODESCHEMES = "/codeschemes";
+
+    public static final String API_PATH_CODES = "/codes";
+
+}

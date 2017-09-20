a3d416f2903045339c13b337da68cc38e341dc62
Miikka Sauramo <miikka.sauramo@phz.fi>
2017-09-20 15:24:34 +0300

    Move constants to one common class

--- src/main/java/fi/vm/yti/cls/common/constants/ApiConstants.java
+++ src/main/java/fi/vm/yti/cls/common/constants/ApiConstants.java
@@ -1,8 +1,39 @@
 package fi.vm.yti.cls.common.constants;
 
 public class ApiConstants {
-    private ApiConstants() {}
+    public static final String SOURCE_INTERNAL = "internal";
 
+    public static final String DATA_CODEREGISTRIES = "coderegistries";
+
+    public static final String DATA_CODESCHEMES = "codeschemes";
+
+    public static final String DATA_CODES = "codes";
+
+    public static final String ELASTIC_INDEX_CODEREGISTRIES = "coderegistries";
+
+    public static final String ELASTIC_INDEX_CODESCHEMES = "codeschemes";
+
+    public static final String ELASTIC_INDEX_CODES = "codes";
+
+    public static final String ELASTIC_TYPE_CODEREGISTRY = "coderegistry";
+
+    public static final String ELASTIC_TYPE_CODESCHEME = "codescheme";
+
+    public static final String ELASTIC_TYPE_CODE = "code";
+
+//    public static final String ELASTIC_INDEX_CODEREGISTRIES = "coderegistries";
+//
+//    public static final String ELASTIC_INDEX_CODESCHEMES = "codeschemes";
+//
+//    public static final String ELASTIC_INDEX_CODES = "codes";
+//
+//    public static final String ELASTIC_TYPE_CODEREGISTRY = "coderegistry";
+//
+//    public static final String ELASTIC_TYPE_CODESCHEME = "codescheme";
+//
+//    public static final String ELASTIC_TYPE_CODE = "code";    
+    
+    
     public static final String API_VERSION = "v1";
 
     public static final String API_BASE_PATH = "/api";
@@ -16,5 +47,15 @@ public class ApiConstants {
     public static final String API_PATH_CODESCHEMES = "/codeschemes";
 
     public static final String API_PATH_CODES = "/codes";
+    
+    public static final String DEFAULT_CODESCHEME_FILENAME = "v1_codeschemes.csv";
+    
+    public static final String DEFAULT_CODEREGISTRY_FILENAME = "v1_coderegistries.csv";
+    
+    public static final String DEFAULT_CODE_FILENAME = "v1_codes.csv";
+    
+    public static final String DEFAULT_CODEREGISTRY_NAME_PREFIX = "testregistry";
+    
+    public static final String DEFAULT_CODESCHEME_NAME = "testscheme";
 
 }

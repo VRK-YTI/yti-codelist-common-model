3b9204879c8e0d36456ab801e52b3f1bac9101c6
Antti Tohmo <antti.tohmo@gofore.com>
2017-09-29 15:21:37 +0300

    Elastic indexing bookkeeping added.

--- src/main/java/fi/vm/yti/codelist/common/constants/ApiConstants.java
+++ src/main/java/fi/vm/yti/codelist/common/constants/ApiConstants.java
@@ -1,50 +1,40 @@
 package fi.vm.yti.codelist.common.constants;
 
 public class ApiConstants {
+
+    public static final String METHOD_GET = "GET";
+    public static final String METHOD_POST = "POST";
+    public static final String METHOD_PUT = "PUT";
+    public static final String METHOD_DELETE = "DELETE";
+    public static final String METHOD_PATCH = "PATCH";
+
     public static final String SOURCE_INTERNAL = "internal";
-
     public static final String DATA_CODEREGISTRIES = "coderegistries";
-
     public static final String DATA_CODESCHEMES = "codeschemes";
-
     public static final String DATA_CODES = "codes";
 
-    public static final String ELASTIC_INDEX_CODEREGISTRIES = "coderegistries";
-
-    public static final String ELASTIC_INDEX_CODESCHEMES = "codeschemes";
-
-    public static final String ELASTIC_INDEX_CODES = "codes";
-
+    public static final String ELASTIC_INDEX_CODELIST = "codelist";
     public static final String ELASTIC_TYPE_CODEREGISTRY = "coderegistry";
-
     public static final String ELASTIC_TYPE_CODESCHEME = "codescheme";
+    public static final String ELASTIC_TYPE_CODE = "code";
 
-    public static final String ELASTIC_TYPE_CODE = "code"; 
-    
     public static final String API_VERSION = "v1";
-
-    public static final String API_BASE_PATH = "/api";
-
     public static final String API_CONTEXT_PATH_INTAKE = "/";
-    
     public static final String API_CONTEXT_PATH_RESTAPI = "";
 
+    public static final String API_BASE_PATH = "/api";
+    public static final String API_PATH_VERSION_V1 = "/v1";
     public static final String API_PATH_CODEREGISTRIES = "/coderegistries";
-
     public static final String API_PATH_CODESCHEMES = "/codeschemes";
-
     public static final String API_PATH_CODES = "/codes";
-    
+    public static final String API_PATH_PING = "/ping";
+    public static final String API_PATH_VERSION = "/version";
+    public static final String API_PATH_SWAGGER_JSON = "/swagger.json";
+
     public static final String DEFAULT_CODESCHEME_FILENAME = "v1_codeschemes.csv";
-    
     public static final String DEFAULT_CODEREGISTRY_FILENAME = "v1_coderegistries.csv";
-    
     public static final String DEFAULT_CODE_FILENAME = "v1_codes.csv";
-    
     public static final String DEFAULT_CODEREGISTRY_NAME_PREFIX = "testregistry";
-    
     public static final String DEFAULT_CODESCHEME_NAME = "testscheme";
-
     public static final String FILTER_NAME_CODEREGISTRY = "codeRegistry";
-
 }

0e09e4e919d44ca8d34923275659c92b9ff7ab0b
Antti Tohmo <antti.tohmo@gofore.com>
2017-10-23 09:41:00 +0300

    Initial unit test for codelist common model.

---
+++ src/test/java/fi/vm/yti/codelist/common/model/MetaTest.java
@@ -0,0 +1,19 @@
+package fi.vm.yti.codelist.common.model;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MetaTest {
+
+    @Test
+    public void testMetaAfter() {
+        final Date isoDate = Meta.parseAfterFromString("2017-10-09");
+        final Calendar date = new GregorianCalendar(2017, Calendar.OCTOBER, 9);
+        assertEquals(isoDate.getTime(), date.getTime().getTime());
+    }
+}

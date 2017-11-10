d7f92c0b06b5e71b22602631ec0174f4ddaeb9e1
Miikka Sauramo <miikka.sauramo@phz.fi>
2017-11-10 14:14:20 +0200

    Add default serialVersionUID

--- src/main/java/fi/vm/yti/codelist/common/model/ServiceClassification.java
+++ src/main/java/fi/vm/yti/codelist/common/model/ServiceClassification.java
@@ -10,6 +10,8 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class ServiceClassification extends Code {
 
+    private static final long serialVersionUID = 1L;
+
     public ServiceClassification() {
     }
 

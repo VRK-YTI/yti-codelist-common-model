c43202768356a2013141f253dcafc09256b79276
Miikka Sauramo <miikka.sauramo@phz.fi>
2017-11-29 15:10:29 +0200

    Remove unused imports.

--- src/main/java/fi/vm/yti/codelist/common/model/Organization.java
+++ src/main/java/fi/vm/yti/codelist/common/model/Organization.java
@@ -4,14 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.MapKeyColumn;
 import javax.persistence.OrderColumn;

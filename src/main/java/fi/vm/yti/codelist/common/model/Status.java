bacfca93efb59e811ebe547bfe567b55650f04a2
Antti Tohmo <antti.tohmo@gofore.com>
2017-12-22 11:29:44 +0200

    Adding SUGGESTED value to statuses.

--- src/main/java/fi/vm/yti/codelist/common/model/Status.java
+++ src/main/java/fi/vm/yti/codelist/common/model/Status.java
@@ -1,7 +1,7 @@
 package fi.vm.yti.codelist.common.model;
 
 /**
- * These are ISO 19135-1 spec status codes for registry status. Service with an added service specific draft status.
+ * These are ISO 19135-1 spec status codes with added service specific draft and suggested statuses.
  *
  * @see <a href="http://inspire.ec.europa.eu/registry/status">http://inspire.ec.europa.eu/registry/status</a>
  */
@@ -12,7 +12,8 @@ public enum Status {
     SUPERSEDED,
     RETIRED,
     DRAFT,
-    SUBMITTED;
+    SUBMITTED,
+    SUGGESTED;
 
     Status() {
     }

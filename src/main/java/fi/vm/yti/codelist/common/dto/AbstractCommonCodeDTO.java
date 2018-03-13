a3ef969af8e1f73a0de722bf00b93baaad3052da
Antti Tohmo <antti.tohmo@gofore.com>
2018-03-13 13:14:24 +0200

    Adding missing JsonView annotation to fix serialization.

--- src/main/java/fi/vm/yti/codelist/common/dto/AbstractCommonCodeDTO.java
+++ src/main/java/fi/vm/yti/codelist/common/dto/AbstractCommonCodeDTO.java
@@ -2,11 +2,16 @@ package fi.vm.yti.codelist.common.dto;
 
 import javax.persistence.MappedSuperclass;
 
+import com.fasterxml.jackson.annotation.JsonView;
+
+import fi.vm.yti.codelist.common.model.Views;
+
 @MappedSuperclass
 abstract class AbstractCommonCodeDTO extends AbstractBaseCodeDTO {
 
     private String codeValue;
 
+    @JsonView(Views.Normal.class)
     public String getCodeValue() {
         return codeValue;
     }

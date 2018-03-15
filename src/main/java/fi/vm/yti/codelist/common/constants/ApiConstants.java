4afc396134e0bc06915bbfa5f343dc3da3ace10c
Antti Tohmo <antti.tohmo@gofore.com>
2018-03-15 23:43:04 +0200

    Admin API path change regarding uri and url rewrites.

--- src/main/java/fi/vm/yti/codelist/common/constants/ApiConstants.java
+++ src/main/java/fi/vm/yti/codelist/common/constants/ApiConstants.java
@@ -47,7 +47,7 @@ public class ApiConstants {
     public static final String API_PATH_ADMIN = "/admin";
     public static final String API_PATH_REINDEX = "/reindex";
     public static final String API_PATH_RELOAD = "/reload";
-    public static final String API_PATH_REWRITEURIS = "/rewriteuris";
+    public static final String API_PATH_REWRITEADDRESSES = "/rewriteaddresses";
     public static final String API_PATH_AUTHENTICATED_USER = "/authenticated-user";
     public static final String API_PATH_CONFIGURATION = "/configuration";
     public static final String API_PATH_CODEREGISTRIES = "/coderegistries";

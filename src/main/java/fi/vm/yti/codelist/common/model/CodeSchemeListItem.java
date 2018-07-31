8b40924c34b5e89f9acec7705fb3f4156fbec390
Tuukka Laitinen <work.ictlaitinen@gmail.com>
2018-07-31 09:45:35 +0300

    YTI-979 - Changes to codelistitem constructor.

--- src/main/java/fi/vm/yti/codelist/common/model/CodeSchemeListItem.java
+++ src/main/java/fi/vm/yti/codelist/common/model/CodeSchemeListItem.java
@@ -1,6 +1,7 @@
 package fi.vm.yti.codelist.common.model;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.Map;
 
 /**
@@ -10,12 +11,18 @@ import java.util.Map;
 public class CodeSchemeListItem implements Serializable {
     private Map<String, String> prefLabel;
     private String uri;
+    private Date startDate;
+    private Date endDate;
+    private String status;
 
     public CodeSchemeListItem() {}
 
-    public CodeSchemeListItem(Map<String, String> prefLabel, String uri) {
+    public CodeSchemeListItem(Map<String, String> prefLabel, String uri, Date startDate, Date endDate, String status) {
         this.prefLabel = prefLabel;
         this.uri = uri;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.status = status;
     }
 
     public Map<String, String> getPrefLabel() {
@@ -32,5 +39,29 @@ public class CodeSchemeListItem implements Serializable {
 
     public void setUri(final String uri) {
         this.uri = uri;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(final Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(final Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(final String status) {
+        this.status = status;
     }
 }

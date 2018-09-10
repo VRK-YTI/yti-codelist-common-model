793c0b2533a819a2203ac2264e3fcec182aee718
Antti Tohmo <antti.tohmo@gofore.com>
2018-09-10 17:14:35 +0300

    Removing unnecessary semicolons.

--- src/main/java/fi/vm/yti/codelist/common/dto/Views.java
+++ src/main/java/fi/vm/yti/codelist/common/dto/Views.java
@@ -5,23 +5,23 @@ public class Views {
     private Views() {
     }
 
-    public static class Normal{};
+    public static class Normal{}
 
-    public static class Extended extends Normal{};
+    public static class Extended extends Normal{}
 
-    public static class ExtendedExternalReference extends Normal{};
+    public static class ExtendedExternalReference extends Normal{}
 
-    public static class ExtendedPropertyType extends Normal{};
+    public static class ExtendedPropertyType extends Normal{}
 
-    public static class ExtendedCode extends Normal{};
+    public static class ExtendedCode extends Normal{}
 
-    public static class ExtendedCodeScheme extends Normal{};
+    public static class ExtendedCodeScheme extends Normal{}
 
-    public static class ExtendedCodeRegistry extends Normal{};
+    public static class ExtendedCodeRegistry extends Normal{}
 
-    public static class ExtendedOrganization extends Normal{};
+    public static class ExtendedOrganization extends Normal{}
 
-    public static class ExtendedExtension extends Normal{};
+    public static class ExtendedExtension extends Normal{}
 
-    public static class ExtendedExtensionScheme extends Normal{};
+    public static class ExtendedExtensionScheme extends Normal{}
 }

920399d3ac4f0b12d717a4078d6f5a76e57dd35b
Antti Tohmo <antti.tohmo@gofore.com>
2018-09-19 13:21:37 +0300

    Bugfixing findbugs issue with date settings.

--- src/main/java/fi/vm/yti/codelist/common/model/CodeSchemeListItem.java
+++ src/main/java/fi/vm/yti/codelist/common/model/CodeSchemeListItem.java
@@ -1,44 +1,51 @@
 package fi.vm.yti.codelist.common.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonView;
-import fi.vm.yti.codelist.common.dto.Views;
-import fi.vm.yti.codelist.common.util.Identifiable;
-import io.swagger.annotations.ApiModelProperty;
-
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.Map;
 import java.util.UUID;
+
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonView;
+
+import fi.vm.yti.codelist.common.dto.Views;
+import fi.vm.yti.codelist.common.util.Identifiable;
+import io.swagger.annotations.ApiModelProperty;
 
 /**
  * This class exists only for the needs of the front-end.
  * It always represents a codescheme but it could be a variant or a version.
  */
 public class CodeSchemeListItem implements Serializable, Identifiable {
+
     private UUID id;
     private Map<String, String> prefLabel;
     private String uri;
     private Date startDate;
-
     private Date endDate;
     private String status;
 
-    public CodeSchemeListItem() {}
+    public CodeSchemeListItem() {
+    }
 
-    public CodeSchemeListItem(UUID id,
-                              Map<String, String> prefLabel,
-                              String uri,
-                              Date startDate,
-                              Date endDate,
-                              String status) {
+    public CodeSchemeListItem(final UUID id,
+                              final Map<String, String> prefLabel,
+                              final String uri,
+                              final Date startDate,
+                              final Date endDate,
+                              final String status) {
         this.id = id;
         this.prefLabel = prefLabel;
         this.uri = uri;
-        this.startDate = startDate;
-        this.endDate = endDate;
+        if (startDate != null) {
+            this.startDate = new Date(startDate.getTime());
+        }
+        if (endDate != null) {
+            this.endDate = new Date(endDate.getTime());
+        }
         this.status = status;
     }
 
@@ -66,15 +73,10 @@ public class CodeSchemeListItem implements Serializable, Identifiable {
         this.uri = uri;
     }
 
-    @ApiModelProperty(
-            dataType = "dateTime"
-    )
-    @JsonFormat(
-            shape = JsonFormat.Shape.STRING,
-            pattern = "yyyy-MM-dd"
-    )
+    @ApiModelProperty(dataType = "dateTime")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     @Temporal(TemporalType.DATE)
-    @JsonView({Views.Normal.class})
+    @JsonView({ Views.Normal.class })
     public Date getStartDate() {
         if (startDate != null) {
             return new Date(startDate.getTime());
@@ -90,15 +92,10 @@ public class CodeSchemeListItem implements Serializable, Identifiable {
         }
     }
 
-    @ApiModelProperty(
-            dataType = "dateTime"
-    )
-    @JsonFormat(
-            shape = JsonFormat.Shape.STRING,
-            pattern = "yyyy-MM-dd"
-    )
+    @ApiModelProperty(dataType = "dateTime")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     @Temporal(TemporalType.DATE)
-    @JsonView({Views.Normal.class})
+    @JsonView({ Views.Normal.class })
     public Date getEndDate() {
         if (endDate != null) {
             return new Date(endDate.getTime());
